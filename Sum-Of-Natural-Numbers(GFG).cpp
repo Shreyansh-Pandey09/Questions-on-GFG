8318cbe557c598f4e4110067583a7a457502b8eb
Shreyansh-Pandey09 <shreyansh.pandey3115@gmail.com>
2025-12-25 15:00:17 +0530

    Create Sum-Of-Natural-Numbers(GFG).cpp

---
+++ Sum-Of-Natural-Numbers(GFG).cpp
@@ -0,0 +1,28 @@
+/*
+Given an integer n. Your task is to calculate the sum of all natural numbers from 1 up to n (inclusive). If n is 0, the sum should be 0.
+
+Examples:
+
+Input: n = 3
+Output: 6
+Explanation: The numbers from 1 to 3 are 1, 2, and 3. Their sum is 1 + 2 + 3 = 6.
+Input: n = 5
+Output: 15
+Explanation: The numbers from 1 to 5 are 1, 2, 3, 4, and 5. Their sum is 1 + 2 + 3 + 4 + 5 = 15.
+Constraints:
+1 ≤ n ≤ 104
+*/
+
+                                                // SOLUTION
+
+class Solution {
+  public:
+    int findSum(int n) {
+        // code here
+        int sum=0;
+        for(int i=0; i<=n; i++){
+            sum = sum+ i;
+        }
+        return sum;
+    }
+};
