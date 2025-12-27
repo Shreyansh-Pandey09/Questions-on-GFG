f0ec44442da79cc7d0f38c378bda792da22f4c1d
Shreyansh-Pandey09 <shreyansh.pandey3115@gmail.com>
2025-12-27 23:01:49 +0530

    Update Floor-Sorted-Array.cpp

--- Floor-Sorted-Array.cpp
+++ Floor-Sorted-Array.cpp
@@ -1,4 +1,3 @@
-@ -0,0 +1,43 @@
 /*
 Given a sorted array arr[] and an integer x, find the index (0-based) of the largest element in arr[] that is less than or equal to x. This element is called the floor of x. If such an element does not exist, return -1.
 
